f64e3f80e1508cfbe02baa638f23271f070ea26f
Saimun Hossain <saimonanam@gmail.com>
2018-10-29 21:28:06 +0600

    product has been displayed in the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HomePage } from './../pages/home/home';
+import { HomePage } from '../pages/home/home';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  WooCommerce: any;
+  products: any[];
+
   constructor(public navCtrl: NavController) {
+
+    this.WooCommerce = WC({
+      url: "http://rafidfeed.com",
+      consumerKey: "ck_d9e1d9540aa7fdc421eae68dc456c18294f54658",
+      consumerSecret: "cs_d5d8c3819670edc5baefcc2fba077d7bc2bb501e"
+    });
+
+    this.WooCommerce.getAsync("products").then((data) => {
+        console.log(JSON.parse(data.body));
+        this.products = JSON.parse(data.body).products;
+    }, (err) => {
+      console.log(err);
+      
+    })
 
   }
 
