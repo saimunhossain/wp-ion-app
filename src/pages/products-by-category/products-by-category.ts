f2f384ac490e33674f65ff950c1ef95fc7b37cf9
Saimun Hossain <saimonanam@gmail.com>
2018-11-03 20:08:59 +0600

    infinite scrolling has been added for category page

--- src/pages/products-by-category/products-by-category.ts
+++ src/pages/products-by-category/products-by-category.ts
@@ -37,4 +37,20 @@ export class ProductsByCategoryPage {
     console.log('ionViewDidLoad ProductsByCategoryPage');
   }
 
+  loadMoreProducts(event){
+      this.page ++;
+      console.log("Getting Page " + this.page);
+      this.WooCommerce.getAsync("products?filter[category]=" + this.category.slug + "&page" + this.page).then((data) => {
+        let temp = (JSON.parse(data.body));
+        
+        this.products = this.products.concat(JSON.parse(data.body).products);
+        console.log(this.products);
+        event.complete();
+
+        if(temp.length < 10)
+        event.enable(false);
+    })
+  }
+
+
 }
