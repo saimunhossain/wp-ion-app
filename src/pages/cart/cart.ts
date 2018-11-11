43fde2321fb969f5f451e02a704008560f246d7e
Saimun Hossain <saimonanam@gmail.com>
2018-11-11 14:30:11 +0600

    cart has been done successfully

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -10,22 +10,59 @@ export class CartPage {
 
   cartItems: any[] = [];
   total: any;
+  showEmptyCartMessage: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public viewCtrl: ViewController) {
+  
+    this.total = 0.0;
     
     this.storage.ready().then(()=>{
 
       this.storage.get("cart").then( (data)=>{
         this.cartItems = data;
         console.log(this.cartItems);
-        
+
+        if(this.cartItems.length > 0){
+
+          this.cartItems.forEach( (item, index)=> {
+            this.total = this.total + (item.product.price * item.qty)
+          })
+
+        } else {
+
+          this.showEmptyCartMessage = true;
+
+        }
+
+
       })
 
-    )}
+    })
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CartPage');
+  removeFromCart(item, i){
+
+    let price = item.product.price;
+    let qty = item.qty;
+
+    this.cartItems.splice(i, 1);
+
+    this.storage.set("cart", this.cartItems).then( ()=> {
+
+      this.total = this.total - (price * qty);
+
+    });
+
+    if(this.cartItems.length == 0){
+      this.showEmptyCartMessage = true;
+    }
+
+
+  }
+
+  closeModal(){
+    this.viewCtrl.dismiss();
   }
 
 }
