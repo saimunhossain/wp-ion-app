a494e9a2ef99506b7ac0a81a19ea5319420b56e0
Saimun Hossain <saimonanam@gmail.com>
2018-11-13 18:46:41 +0600

    email velidation has been done

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-signup',
@@ -9,12 +10,19 @@ export class SignupPage {
 
   newUser: any = {};
   billing_shipping_same: boolean;
+  WooCommerce:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
   
     this.newUser.billing_address = {};
     this.newUser.shipping_address = {};
     this.billing_shipping_same = false;
+
+    this.WooCommerce = WC({
+      url: "http://rafidfeed.com",
+      consumerKey: "ck_d9e1d9540aa7fdc421eae68dc456c18294f54658",
+      consumerSecret: "cs_d5d8c3819670edc5baefcc2fba077d7bc2bb501e"
+    });
   }
 
   ionViewDidLoad() {
@@ -23,6 +31,41 @@ export class SignupPage {
 
   setBillingToShipping(){
     this.billing_shipping_same = !this.billing_shipping_same;
+  }
+
+  checkEmail(){
+    let validEmail = false;
+    let reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(reg.test(this.newUser.email)){
+      //email looks valid
+       this.WooCommerce.getAsync('customers/email/' + this.newUser.email).then( (data) => {
+        let res = (JSON.parse(data.body));
+         if(res.errors){
+          validEmail = true;
+           this.toastCtrl.create({
+            message: "Congratulations. Email is good to go.",
+            duration: 3000
+          }).present();
+         } else {
+          validEmail = false;
+           this.toastCtrl.create({
+            message: "Email already registered. Please check.",
+            showCloseButton: true
+          }).present();
+        }
+         console.log(validEmail);
+       })
+     } else {
+      validEmail = false;
+      this.toastCtrl.create({
+        message: "Invalid Email. Please check.",
+        showCloseButton: true
+      }).present();
+      console.log(validEmail);
+    }
+  
+
   }
 
 }
