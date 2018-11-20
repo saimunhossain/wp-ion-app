ad0b47db6c5f81b1c21c9d2464ec9aa0a8f11f46
Saimun Hossain <saimonanam@gmail.com>
2018-11-20 19:42:35 +0600

    checkout pgae filled up with login user

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-checkout',
@@ -8,12 +9,14 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CheckoutPage {
 
+  WooCommerce: any;
   newOrder: any;
   paymentMethods: any[];
   patmentMethod: any;
   billing_shipping_same: boolean;
+  userInfo: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
     this.newOrder = {};
     this.newOrder.billing_address = {};
     this.newOrder.shipping_address = {};
@@ -25,6 +28,21 @@ export class CheckoutPage {
       { method_id: "cod", method_title: "Cash on Delivery" },
       { method_id: "paypal", method_title: "PayPal" }
     ]
+
+    this.WooCommerce = WC({
+      url: "http://rafidfeed.com",
+      consumerKey: "ck_d9e1d9540aa7fdc421eae68dc456c18294f54658",
+      consumerSecret: "cs_d5d8c3819670edc5baefcc2fba077d7bc2bb501e"
+    });
+
+    this.storage.get("userLoginInfo").then( (userLoginInfo) => {
+        this.userInfo = userLoginInfo.user;
+
+        let email = userLoginInfo.user.email;
+        this.WooCommerce.getAsync("customers/email/"+email).then( (data) => {
+          this.newOrder = JSON.parse(data.body).customer;
+        })
+    })
   }
 
   setBillingToShipping(){
@@ -35,7 +53,7 @@ export class CheckoutPage {
       this.newOrder.shipping_address = this.newOrder.billing_address;
     }
    }
-
+ 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CheckoutPage');
   }
