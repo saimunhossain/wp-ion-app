98361766b6f0b5356c98314e4e9225e417483524
Saimun Hossain <saimonanam@gmail.com>
2018-11-26 22:02:27 +0600

    according to search result showed products on search page

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-search',
@@ -7,8 +8,23 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
+  searchQuery: string = "";
+  WooCommerce: any;
+  products: any[] = [];
+  page: number = 2;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log(this.navParams.get("searchQuery"));
+    // console.log(this.navParams.get("searchQuery"));
+
+    this.searchQuery = this.navParams.get("searchQuery");
+    this.WooCommerce = WC({
+      url: "http://rafidfeed.com",
+      consumerKey: "ck_d9e1d9540aa7fdc421eae68dc456c18294f54658",
+      consumerSecret: "cs_d5d8c3819670edc5baefcc2fba077d7bc2bb501e"
+    });
+     this.WooCommerce.getAsync("products?filter[q]=" + this.searchQuery).then((searchData) => {
+      this.products = JSON.parse(searchData.body).products;
+    });
     
   }
 
